22caba497b62b7535752e9f0dd4a9f0438cc6209
Stefano Moro <stefano.moro@ironhack.com>
2023-11-29 15:22:11 +0100

    calculator: sub done

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ second_number = input('Enter second number: ');
 sum = float(first_number) + float(second_number);
 # Print sum
 print('Sum: ' + str(sum));
+
+# Calculator SUB
+# Get first number
+first_number = input('Enter first number: ');
+# Get second number
+second_number = input('Enter second number: ');
+# Calculate sub
+sub = float(first_number) - float(second_number);
+# Print sub
+print('Sub: ' + str(sub));

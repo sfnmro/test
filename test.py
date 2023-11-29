13daa86e19dd4f2156e5d2b9b1b66248db6e27f3
Stefano Moro <stefano.moro@ironhack.com>
2023-11-29 15:23:24 +0100

    calculator: mult done

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ second_number = input('Enter second number: ');
 sum = float(first_number) + float(second_number);
 # Print sum
 print('Sum: ' + str(sum));
+
+# Calculator (MULT)
+# Get first number
+first_number = input('Enter first number: ');
+# Get second number
+second_number = input('Enter second number: ');
+# Calculate mult
+mult = float(first_number) * float(second_number);
+# Print mult
+print('Mult: ' + str(mult));
